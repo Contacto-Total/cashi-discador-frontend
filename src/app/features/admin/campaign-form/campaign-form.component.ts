62d908bf265c77b4823e3f0988cec6f56010421c
ContactoTotalQA <ct_samuel_ch@hotmail.com>
2026-04-06 13:00:55 -0500

    feat: Correccion de fechas

--- src/app/features/admin/campaign-form/campaign-form.component.ts
+++ src/app/features/admin/campaign-form/campaign-form.component.ts
@@ -13,17 +13,13 @@ import { Portfolio, SubPortfolio } from '../../../maintenance/models/portfolio.m
 import { environment } from '../../../../environments/environment';
 import { Workbook, Worksheet } from 'exceljs';
 import { saveAs } from 'file-saver';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-campaign-form',
   standalone: true,
   imports: [CommonModule, FormsModule, LucideAngularModule],
   templateUrl: './campaign-form.component.html',
-  styleUrls: ['./campaign-form.component.css'],
-  providers: [
-  { provide: MAT_DATE_LOCALE, useValue: 'es-PE' }
-]
+  styleUrls: ['./campaign-form.component.css']
 })
 export class CampaignFormComponent implements OnInit {
   campaign: Campaign = {
